f4c67a329c9da5d3906e12e3879ed3c273ea3875
John Swinbank <john@swinbank.org>
2020-06-06 16:13:29 -0700

    Make Eups fetch all tags

--- rubin_changelog/eups.py
+++ rubin_changelog/eups.py
@@ -1,13 +1,14 @@
-import yaml
+import re
 
 from datetime import datetime
-from typing import Dict, Set
 from urllib.request import urlopen
+from collections import Mapping
 
-from typing import Iterable
+from typing import Any, Iterable, Iterator, List
 
-from .config import EUPS_PKGROOT, TARGET_DIR, DEBUG
-from .repository import Repository
+from lxml import html
+
+from .config import EUPS_PKGROOT
 
 
 class EupsTag(object):
@@ -17,15 +18,28 @@ class EupsTag(object):
         self.products = products
 
 
-class Eups(object):
-    def __init__(self, *, pkgroot: str = EUPS_PKGROOT):
+class Eups(Mapping):
+    def __init__(self, *, pkgroot: str = EUPS_PKGROOT, pattern: str = "w_20"):
         self._pkgroot = pkgroot
-        self._tags = {}
+        self._pattern = pattern
+        self._tags = {
+            tag_name: self.__retrieve_tag(tag_name)
+            for tag_name in self.__retrieve_tag_list()
+        }
+
+    def __retrieve_tag_list(self) -> List[str]:
+        h = html.parse(urlopen(self._pkgroot + "/tags"))
+        return [
+            el.text[:-5]
+            for el in h.findall("./body/table/tr/td/a")
+            if el.text[-5:] == ".list" and re.match(self._pattern, el.text)
+        ]
 
     def __retrieve_tag(self, tag_name: str) -> EupsTag:
         u = urlopen(f"{self._pkgroot}/tags/{tag_name}.list")
-        tag_date = datetime.strptime(u.info()['last-modified'],
-                                             "%a, %d %b %Y %H:%M:%S %Z")
+        tag_date = datetime.strptime(
+            u.info()["last-modified"], "%a, %d %b %Y %H:%M:%S %Z"
+        )
         products = []
         for line in u.read().decode("utf-8").strip().split("\n"):
             if line.startswith("EUPS distribution "):
@@ -37,6 +51,10 @@ class Eups(object):
         return EupsTag(tag_name, tag_date, products)
 
     def __getitem__(self, tag_name: str) -> EupsTag:
-        if tag_name not in self._tags:
-            self._tags[tag_name] = self.__retrieve_tag(tag_name)
         return self._tags[tag_name]
+
+    def __iter__(self) -> Iterator[Any]:
+        return iter(self._tags)
+
+    def __len__(self) -> int:
+        return len(self._tags)
